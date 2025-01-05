6edac9e1adae704241e90ccb1116df7e855fcce2
lakkenlak <remoldpolka@gmail.com>
2025-01-05 16:11:32 +0100

    add base to manifest url

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import { setStore } from '$lib/store.svelte';
 	import '../app.css';
 	let { children } = $props();
@@ -8,7 +9,7 @@
 
 <svelte:head>
 	<title>Vaegt</title>
-	<link rel="manifest" href="/manifest.json" />
+	<link rel="manifest" href="{base}/manifest.json" />
 </svelte:head>
 
 {@render children()}
